305967766cbfa0a78655af79fd67b6d5055427db
markus <markus.lilienberg@glesys.se>
2020-04-15 12:51:35 +0200

    Inject ServiceProvider

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -2,6 +2,7 @@
 
 namespace Butler\Service\Foundation;
 
+use Butler\Service\ServiceProvider;
 use Illuminate\Foundation\Application as BaseApplication;
 
 class Application extends BaseApplication
@@ -9,5 +10,12 @@ class Application extends BaseApplication
     public function configPath($path = '')
     {
         return realpath(__DIR__ . '/../config');
+    }
+
+    public function registerConfiguredProviders()
+    {
+        $this->register(new ServiceProvider($this));
+
+        parent::registerConfiguredProviders();
     }
 }
