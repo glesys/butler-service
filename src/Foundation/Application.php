f857e1532460f531736c2e164a68510444cbea23
markus <markus.lilienberg@glesys.se>
2020-06-10 10:36:35 +0200

    Add BUTLER_SERVICE_VERSION constant

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
+    public const BUTLER_SERVICE_VERSION = '0.2.12';
+
     public function configPath($path = '')
     {
         return realpath(__DIR__ . '/../config');
