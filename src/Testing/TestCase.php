d2bf1fbc6f0325e3dcfe81e5a34c67df689b0544
markus <markus.lilienberg@glesys.se>
2020-08-03 09:58:44 +0200

    Add actingAsJwtUser helper for tests

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use Concerns\CreatesApplication;
+    use Concerns\InteractsWithAuthentication;
     use Concerns\MakesGraphqlRequests;
     use Concerns\MigratesDatabases;
 }
