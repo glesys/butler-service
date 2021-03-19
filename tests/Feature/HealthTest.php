98b3a11386c851580ad0129241653d50eebcf2bc
markus <markus.lilienberg@glesys.se>
2021-03-19 14:04:33 +0100

    Use AssertableJson in tests

--- tests/Feature/HealthTest.php
+++ tests/Feature/HealthTest.php
@@ -1,8 +1,11 @@
 <?php
 
+// phpcs:disable PSR2.Methods.FunctionCallSignature.SpaceBeforeCloseBracket
+
 namespace Butler\Service\Tests\Feature;
 
 use Butler\Service\Tests\TestCase;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 class HealthTest extends TestCase
 {
@@ -10,29 +13,17 @@ class HealthTest extends TestCase
     {
         $this->getJson(route('health'))
             ->assertOk()
-            ->assertJsonStructure([
-                'service' => [
-                    'name',
-                    'php',
-                    'laravel',
-                    'butlerService',
-                    'timezone',
-                ],
-                'checks' => [
-                    [
-                        'name',
-                        'slug',
-                        'group',
-                        'description',
-                        'result' => [
-                            'message',
-                            'state',
-                            'value',
-                        ],
-                    ],
-                ],
-            ])
-            ->assertJsonPath('service.name', config('app.name'))
-            ->assertJsonCount(4, 'checks');
+            ->assertJson(fn (AssertableJson $json) => $json
+                ->has('service', fn (AssertableJson $json) => $json
+                    ->hasAll('php', 'laravel', 'butlerService', 'timezone')
+                    ->where('name', config('app.name'))
+                )
+                ->has('checks', 4, fn (AssertableJson $json) => $json
+                    ->hasAll('name', 'slug', 'group', 'description')
+                    ->has('result', fn (AssertableJson $json) => $json
+                        ->hasAll('message', 'state', 'value')
+                    )
+                )
+            );
     }
 }
