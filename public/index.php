911132923949bb80c40cd6c07a995fbdaebb2bfe
markus <markus.lilienberg@glesys.se>
2021-12-08 11:10:20 +0100

    Add publishable index.php

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle($request = Request::capture())->send();
+
+$kernel->terminate($request, $response);
