298579d7701cf28be9465cc2bbb15bd93dbdfce4
markus <markus.lilienberg@glesys.se>
2021-12-21 00:49:13 +0100

    Unguard model instead of setting guarded property to empty array

--- tests/Database/ModelTest.php
+++ tests/Database/ModelTest.php
@@ -7,24 +7,21 @@ use Butler\Service\Tests\TestCase;
 
 class ModelTest extends TestCase
 {
-    private $model;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->model = new class extends Model {
-            //
-        };
-    }
-
     public function test_connection_name()
     {
-        $this->assertEquals('default', $this->model->getConnectionName());
+        $model = new class extends Model {
+            //
+        };
+
+        $this->assertEquals('default', $model->getConnectionName());
     }
 
     public function test_is_unguarded()
     {
-        $this->assertEquals([], $this->model->getGuarded());
+        $model = new class extends Model {
+            //
+        };
+
+        $this->assertTrue($model->isUnguarded());
     }
 }
