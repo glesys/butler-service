0b7b9d67af45ad91edb0d98cbd500325184b36cc
markus <markus.lilienberg@glesys.se>
2022-05-13 11:01:35 +0200

    Dont use PreventRequestsDuringMaintenance middleware

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -16,7 +16,6 @@ abstract class Kernel extends HttpKernel
     protected $middleware = [
         \Illuminate\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
-        \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
