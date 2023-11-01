b2f93194bf41711880c3fd5a43c57418ca96ab2e
markus <markus.lilienberg@glesys.se>
2023-11-01 21:20:53 +0100

    Update card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,14 @@
-@props(['title', 'buttons'])
-<div {{ $attributes->class('border border-gray-100 dark:border-gray-500 bg-white dark:bg-gray-800 rounded mb-5') }}>
+@props([
+  'title',
+  'buttons',
+  'noWrap' => false,
+  'noPadding' => false,
+])
 
+<div {{ $attributes->class([
+  'border border-gray-100 dark:border-gray-500 bg-white dark:bg-gray-800 rounded mb-5',
+  'whitespace-nowrap overflow-x-auto' => $noWrap,
+]) }}>
   <div class="flex items-center justify-between h-14 py-3 px-4 -mb-2 border-b border-gray-100 dark:border-gray-500">
     <x-butler::h1>{{ $title }}</x-butler::h1>
 
@@ -9,5 +17,5 @@
     @endisset
   </div>
 
-  <div class="p-8 mb-1">{{ $slot }}</div>
+  <div @class(['mb-1', 'p-8' => $noPadding === false])>{{ $slot }}</div>
 </div>
