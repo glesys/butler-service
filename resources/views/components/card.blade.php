280f4f92b78178feb6dc4910eb78ddd6e00db8a9
markus <markus.lilienberg@glesys.se>
2023-11-14 15:08:12 +0100

    Use tailwind-scrollbar

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -5,11 +5,8 @@
   'noPadding' => false,
 ])
 
-<div {{ $attributes->class([
-  'border border-gray-100 dark:border-gray-500 bg-white dark:bg-gray-800 rounded mb-5',
-  'whitespace-nowrap overflow-x-auto' => $noWrap,
-]) }}>
-  <div class="flex items-center justify-between h-14 py-3 px-4 -mb-2 border-b border-gray-100 dark:border-gray-500">
+<div {{ $attributes->class(['border border-gray-100 dark:border-gray-500 bg-white dark:bg-gray-800 rounded mb-5']) }}>
+  <div class="flex items-center justify-between h-14 py-3 px-4 border-b border-gray-100 dark:border-gray-500">
     <x-butler::h1>{{ $title }}</x-butler::h1>
 
     @isset($buttons)
@@ -17,5 +14,9 @@
     @endisset
   </div>
 
-  <div @class(['mb-1', 'p-8' => $noPadding === false])>{{ $slot }}</div>
+  <div @class([
+    'whitespace-nowrap overflow-x-auto scrollbar-thin scrollbar-track-rounded scrollbar-thumb-rounded scrollbar-thumb-gray-400 scrollbar-track-white dark:scrollbar-thumb-gray-900 dark:scrollbar-track-gray-800' => $noWrap,
+  ])>
+    <div @class(['mb-1', 'p-8' => $noPadding === false])>{{ $slot }}</div>
+  </div>
 </div>
