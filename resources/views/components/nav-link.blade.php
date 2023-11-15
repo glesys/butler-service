f631befe8e58aed2d37e2af4bee267a5592f5c40
markus <markus.lilienberg@glesys.se>
2023-11-15 16:46:23 +0100

    Fix nav-link component

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,8 +3,8 @@
   'active' => false,
 ])
 <a {{ $attributes->merge(['href' => $route ? route($route) : '#'])->class([
-  'py-3 px-1 text-gray-500/70 dark:text-gray-400 border-b-2 border-white dark:border-gray-800',
+  'py-3 px-1 text-gray-500/70 dark:text-gray-400 border-b-2',
   ($active)
     ? 'dark:text-white border-blue-light'
-    : 'hover:text-gray-400 dark:hover:text-gray-300 hover:border-gray-400'
+    : 'border-white dark:border-gray-800 hover:text-gray-400 dark:hover:text-gray-300 hover:border-gray-400'
 ]) }}>{{ $slot }}</a>
