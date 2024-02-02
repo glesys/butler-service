4ce77ab54d6508eb18cbb434cabb02d0f27e999a
markus <markus.lilienberg@glesys.se>
2024-02-02 15:58:08 +0100

    Type hint in MakesGraphqlRequests

--- src/Testing/Concerns/MakesGraphqlRequests.php
+++ src/Testing/Concerns/MakesGraphqlRequests.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Butler\Service\Testing\Concerns;
 
+use Illuminate\Testing\TestResponse;
+
 trait MakesGraphqlRequests
 {
-    public function graphql(string $query, array $variables = [])
+    public function graphql(string $query, array $variables = []): TestResponse
     {
         return $this->postJson(route('graphql'), compact('query', 'variables'));
     }
