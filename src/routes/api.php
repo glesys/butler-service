f47a8e0d3820716a39cfe18515223c2ccda616bd
markus <markus.lilienberg@glesys.se>
2024-08-02 11:23:53 +0200

    remove unused route files

--- src/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Butler\Service\Http\Controllers\GraphqlController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('graphql', config('butler.service.controllers.graphql', GraphqlController::class))->name('graphql');
