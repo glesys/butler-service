749165683413f242902c2ba296488b68f187c7cb
markus <markus.lilienberg@glesys.se>
2024-08-02 11:21:51 +0200

    declare strict type

--- src/Socialite/FakeProvider.php
+++ src/Socialite/FakeProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Butler\Service\Socialite;
 
 class FakeProvider extends PassportProvider
--- src/Socialite/PassportProvider.php
+++ src/Socialite/PassportProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Butler\Service\Socialite;
 
 use GuzzleHttp\RequestOptions;
