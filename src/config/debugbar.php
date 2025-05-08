270e0cda75afdb76f8d61427ee90c83c231a0282
Christoffer Andersson <christoffer.andersson@glesys.com>
2025-05-08 11:03:24 +0200

    Update pint.json

--- src/config/debugbar.php
+++ src/config/debugbar.php
@@ -205,7 +205,7 @@ return [
         ],
         'views' => [
             'timeline' => false,  // Add the views to the timeline (Experimental)
-            'data' => false,    //Note: Can slow down the application, because the data can be quite large..
+            'data' => false,    // Note: Can slow down the application, because the data can be quite large..
         ],
         'route' => [
             'label' => true,  // show complete route on bar
