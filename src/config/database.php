0a624876dbae20ef1c4d94abc25b89bdc31d9c56
Olle Svensson <olle.svensson@glesys.se>
2026-03-13 13:15:01 +0100

    Make database config option compatible with PHP 8.5
    PDO::MYSQL_ATTR_SSL_CA constant is being deprecated.

--- src/config/database.php
+++ src/config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+use Pdo\Mysql;
 
 return [
 
@@ -48,7 +49,7 @@ return [
             'engine' => null,
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
